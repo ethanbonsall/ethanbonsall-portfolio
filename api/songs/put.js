af0c00b5e9a2fe471896fe8d67a4a2ab8b7a0dcc
ethanbonsall <ethanbonsall50@gmail.com>
2025-02-22 20:47:02 -0500

    pls

--- api/songs/put.js
+++ api/songs/put.js
@@ -1,4 +1,4 @@
-import { supabase } from "../../supabaseClient.js";
+import { supabase } from "src/utils/supabaseClient.js";
 
 export default async function handler(req, res) {
   if (req.method !== "PUT") {
