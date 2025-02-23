097cfa0b28efb62f1cf864cceaa4e641acf39905
ethanbonsall <ethanbonsall50@gmail.com>
2025-02-22 19:30:00 -0500

    changed to supabase on vercel

--- supabaseClient.js
+++ supabaseClient.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const supabaseUrl = "https://zlntgnoeilkukxjhhkav.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpsbnRnbm9laWxrdWt4amhoa2F2Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0MDI2NzU2OCwiZXhwIjoyMDU1ODQzNTY4fQ.5Rhtos2H5UXX1P6G6zJq_688JhcfKoQTsXNlCfjy6q0";
+const supabaseUrl = "ttps://gvekpnebsukwwwwiczzq.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd2ZWtwbmVic3Vrd3d3d2ljenpxIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0MDI3MDEyOSwiZXhwIjoyMDU1ODQ2MTI5fQ.j7UP5xwjBrFOEx6Qn9-8fdrptqlqOWASxOh59To9J_Y";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
