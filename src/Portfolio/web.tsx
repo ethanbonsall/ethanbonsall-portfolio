3a9f79fa282b5e1f039383088067bbfdd677373f
ethanbonsall <ethanbonsall50@gmail.com>
2025-04-04 17:26:36 -0400

    Please work

--- src/Portfolio/web.tsx
+++ src/Portfolio/web.tsx
@@ -68,8 +68,7 @@ const Web = () => {
               key={index}
               className="group relative rounded-xl border-2 shadow-md overflow-visible"
             >
-              {/* Zoomable image wrapper */}
-              <div className="relative z-0">
+              <div className="relative z-0 group-hover:scale-125 group-hover:z-10 group-hover:relative">
                 <a
                   href={projects[index].link}
                   target="_blank"
@@ -79,29 +78,28 @@ const Web = () => {
                   <img
                     src={photo}
                     alt={`Project ${index}`}
-                    className="w-full h-auto object-cover transition-transform duration-300 group-hover:scale-125 group-hover:z-10 group-hover:relative"
+                    className="w-full h-auto object-cover transition-transform duration-300"
                   />
                 </a>
-              </div>
 
-              {/* Description (hidden until hover) */}
-              <div className="max-h-0 overflow-hidden group-hover:max-h-[300px] transition-all duration-300">
-                <div className="bg-[#f5efe7] text-[#213555] text-sm p-3 flex justify-between items-center mt-1 rounded-b-xl">
-                  <p className="whitespace-pre-wrap text-left flex-1 mr-2">
-                    {projects[index].description}
-                  </p>
-                  <a
-                    href={projects[index].git}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="flex-shrink-0"
-                  >
-                    <img
-                      src={githubLogo}
-                      alt="GitHub"
-                      className="w-6 h-6 hover:scale-110 transition-transform"
-                    />
-                  </a>
+                <div className="max-h-0 overflow-hidden group-hover:max-h-[300px] transition-all duration-300">
+                  <div className="bg-[#f5efe7] text-[#213555] text-sm p-3 flex justify-between items-center mt-1 rounded-b-xl">
+                    <p className="whitespace-pre-wrap text-left flex-1 mr-2">
+                      {projects[index].description}
+                    </p>
+                    <a
+                      href={projects[index].git}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="flex-shrink-0"
+                    >
+                      <img
+                        src={githubLogo}
+                        alt="GitHub"
+                        className="w-6 h-6 hover:scale-110 transition-transform"
+                      />
+                    </a>
+                  </div>
                 </div>
               </div>
             </div>
