2ce491dd3fd91b57aa2ea49c733abc1ab03ee60f
ethanbonsall <ethanbonsall50@gmail.com>
2025-04-10 21:28:04 -0400

    Fixed hydration error

--- web/components/Portfolio/experience.tsx
+++ web/components/Portfolio/experience.tsx
@@ -1,8 +1,15 @@
+import { useEffect, useState } from "react";
+
 const Experience = () => {
+  const [durations, setDurations] = useState({
+    sheetz: "",
+    cs: "",
+    mhp: "",
+  });
+
   const getDuration = (startDate: Date, endDate: Date = new Date()) => {
     const start = new Date(startDate);
     const end = new Date(endDate);
-
     let years = end.getFullYear() - start.getFullYear();
     let months = end.getMonth() - start.getMonth();
 
@@ -16,6 +23,15 @@ const Experience = () => {
 
     return [yearStr, monthStr].filter(Boolean).join(" ");
   };
+
+  useEffect(() => {
+    setDurations({
+      sheetz: getDuration(new Date("2025-04-01")),
+      cs: getDuration(new Date("2024-09-01")),
+      mhp: getDuration(new Date("2023-07-01")),
+    });
+  }, []);
+
   return (
     <section className="bg-[#d8c4b6] text-[#213555] rounded-xl p-5 mt-6 w-[93%] mb-2 border-2">
       <h1 className="text-4xl font-bold">Experience</h1>
@@ -26,18 +42,14 @@ const Experience = () => {
         <p className="text-lg font-medium italic">
           Associate Programmer Intern
         </p>
-        <p className="text-md">
-          Apr 2025 – Present · {getDuration(new Date("2025-04-01"))}
-        </p>
+        <p className="text-md">Apr 2025 – Present · {durations.sheetz}</p>
         <p className="text-sm">Internship</p>
       </div>
 
       <div className="mb-6">
         <h2 className="text-2xl font-semibold">UNC CS + Social Good</h2>
         <p className="text-lg font-medium italic">Frontend Developer</p>
-        <p className="text-md">
-          Sep 2024 – Present · {getDuration(new Date("2024-09-01"))}
-        </p>
+        <p className="text-md">Sep 2024 – Present · {durations.cs}</p>
         <p className="text-sm">Internship</p>
         <ul className="list-disc pl-5 mt-1 text-md">
           <li>Developed websites using Supabase, NW.js, and React</li>
@@ -51,9 +63,7 @@ const Experience = () => {
       <div className="mb-6">
         <h2 className="text-2xl font-semibold">Magic Hour Portraits</h2>
         <p className="text-lg font-medium italic">Web Developer</p>
-        <p className="text-md">
-          Jul 2023 – Present · {getDuration(new Date("2023-07-01"))}
-        </p>
+        <p className="text-md">Jul 2023 – Present · {durations.mhp}</p>
         <p className="text-sm">Contract · Remote · Camp Hill, Pennsylvania</p>
         <ul className="list-disc pl-5 mt-1 text-md">
           <li>
@@ -64,4 +74,5 @@ const Experience = () => {
     </section>
   );
 };
+
 export default Experience;
