70fc65872b22423bcfe87a69d579541f5ffd67fc
ethanbonsall <ethanbonsall50@gmail.com>
2025-04-25 11:59:50 -0400

    Added analytics

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -2,6 +2,8 @@ import { ThemeProvider } from "@/context/themecontext";
 import "@/styles/globals.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react"
+
 
 const queryClient = new QueryClient();
 
@@ -9,6 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider>
+        <Analytics />
         <Component {...pageProps} />
       </ThemeProvider>
     </QueryClientProvider>
